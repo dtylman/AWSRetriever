899eb33a880b33bffa311cb7a6ff96587ab392d2
Danny Tylman <dtylman@gmail.com>
2018-12-23 15:11:15 +0000

    dev

---
+++ CloudOps/Operations/Opertaion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Amazon;
+using Amazon.Runtime;
+
+namespace CloudOps.Operations
+{
+    public abstract class Operation
+    {
+
+        public virtual bool SupportsRegion(RegionEndpoint region)
+        {
+            return true; //override when needed
+        }
+
+        public abstract void Invoke(AWSCredentials creds, RegionEndpoint region, int maxItems);
+
+    }
+}
---
+++ CloudOps/Scanner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Amazon;
+using Amazon.Runtime;
+using CloudOps.Operations;
+
+namespace CloudOps
+{
+    public class Scanner
+    {
+        private readonly List<Operation> operations;
+        private readonly AWSCredentials creds;
+        private readonly int maxItems = 100;
+
+        public Scanner()
+        {
+            this.operations = new List<Operation>
+            {
+                new LambaListFunctionsOperation()
+            };
+        }
+
+        void Scan()
+        {
+            List<RegionEndpoint> regions = new List<RegionEndpoint>(RegionEndpoint.EnumerableAllRegions);
+            foreach (Operation op in this.operations)
+            {
+                foreach (RegionEndpoint region in regions)
+                {
+                    if (op.SupportsRegion(region))
+                    {
+                        op.Invoke(this.creds, region, this.maxItems);
+                    }
+                }
+            }
+        }
+    }
+}
